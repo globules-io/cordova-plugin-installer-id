560af8d594137582bf825c87cc77852784ae0e9c
Eric <info@ericparlier.com>
2026-02-01 09:34:13 +0100

    Added sideLoaded method

--- www/InstallerId.js
+++ www/InstallerId.js
@@ -2,3 +2,8 @@ var exec = require('cordova/exec');
 exports.getInstaller = function (success, error) {
      exec(success, error, 'InstallerId', 'getInstaller', []);
 };
+exports.sideLoaded = function(success, error) {
+     exec((res)=>{
+          success(!res || res === '');          
+     }, error, 'InstallerId', 'getInstaller', []);
+};
